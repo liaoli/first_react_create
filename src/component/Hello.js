0b6f02a2b7bb1bb9cfef717ebb3b8b426bc8bc66
liaoli <277218962@qq.com>
2023-10-11 13:42:23 +0800

    组件分离

---
+++ src/component/Hello.js
@@ -0,0 +1,14 @@
+import React from "react"
+import '../css/index.css'
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <h1 className="title">
+                Hello, I'm a Class component!
+            </h1>
+        )
+    }
+}
+
+export default Hello
