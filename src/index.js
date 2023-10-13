a60562182ab3c3aeb95763bb0a584da2354d54ee
liaoli <277218962@qq.com>
2023-10-13 18:29:51 +0800

    路由 	# Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // 2 导入组件：
+
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const First = () => <p>页面一的内容</p>
-
+const socend = () => <p>页面二的内容</p>
 const App = () => (
     <Router>
-    <div>
-        <h1>React Router</h1>
-        <ul>
-            <li><Link to="/">页面一</Link></li>
-            <li><Link to="/page2">页面二</Link></li>
-        </ul>
-        <hr />
-        <Route exact path="/" component={First} />
-        <Route path="/page2" component={First} />   
-    </div>
+        <div>
+            <Route exact path="/first" component={First} />
+            <h1>React Router</h1>
+            <Link to="/first">页面一</Link>
+            <br />
+            <Link to="/socend">页面二</Link>
+
+
+            <Route exact path="/socend" component={socend} />
+        </div>
     </Router>
 )
 
