76f8ba9edc1b1c4b6dadbf52da38e0c5e495fc3a
liaoli <277218962@qq.com>
2023-10-13 17:42:39 +0800

    router start

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-/* 
-  虚拟DOM 和 Diff算法
-*/
+// 2 导入组件：
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-// 生成随机数
-class App extends React.PureComponent {
-  state = {
-    number: 0
-  }
+const First = () => <p>页面一的内容</p>
 
-  handleClick = () => {
-    this.setState(() => {
-      return {
-        number: Math.floor(Math.random() * 2)
-      }
-    })
-  }
-
-  // render方法调用并不意味着浏览器中的重新渲染！！！
-  // render方法调用仅仅说明要进行diff
-  render() {
-    const el = (
-      <div>
-        <h1>随机数：</h1>
-        <p>{this.state.number}</p>
-        <button onClick={this.handleClick}>重新生成</button>
-      </div>
-    )
-    console.log(el)
-
-    return el
-  }
-}
+const App = () => (
+    <Router>
+    <div>
+        <h1>React Router</h1>
+        <ul>
+            <li><Link to="/">页面一</Link></li>
+            <li><Link to="/page2">页面二</Link></li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={First} />
+        <Route path="/page2" component={First} />   
+    </div>
+    </Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
